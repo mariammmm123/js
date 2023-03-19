2b8b506e8e0ef299bcce8c6e2682d25bf5f7b82f
mariammmm123 <125983001+mariammmm123@users.noreply.github.com>
2023-03-19 13:15:32 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//----1------//
+
+// program to check if the number is even or odd
+// take input from the user
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
+
+// Gets user input
+var name = prompt("What is your name?");
+var num = prompt("What is your favorite number? ");
+
+//-----2------//
+
+
+
+
+
+
+//-----3------//
+
+// let fruit = ['appel', 'pear', 'banana', 'grape', mango']
+// console.log(fruit.pop())
+
+// console.log(fruit.push('cherry'))
+// console.log(fruit)
+
+// console.log(fruit.shift())
+// console.log(fruit)
+// console.log(fruit.unshift('kiwi'))
+// console.log(fruit)
+
+
+//----4-----//
+const family = {
+    surname: 'Brown',
+    members: [
+        {
+            role: 'father',
+            name: 'George',
+            interests: ['cars', 'soccer']
+        },
+        {
+            role: 'mother',
+            name: 'Mary',
+            interests: ['movies']
+        },
+        {
+            role: 'child',
+            name: 'Tom',
+            interests: ['basketball', 'cartoons']
+        }
+    ]
+   }
+     console.log(family.members[2].interests[0])
+
+
+
